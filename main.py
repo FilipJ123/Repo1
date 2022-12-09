5c8d13d3ae6fdd4a5cb0a4ec5b217b0ba8539f69
uczen <uczen@ckziu.jaworzno.pl>
2022-12-09 13:39:06 +0100

    Commicig

---
+++ main.py
@@ -0,0 +1,22 @@
+key_ = 0
+def code(txt):
+    coded = ""
+
+
+    for i in range(len(txt)):
+        key_ = i+1
+        if ord(txt[i]) > 122 - key_:
+            coded += chr(ord(txt[i]) + key_ - 26)
+        else:
+            coded += chr(ord(txt[i]) + key_)
+    return coded
+
+def main(args):
+
+    tekst = input()
+    print("Zaszyfrowany:\n", code(tekst))
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
